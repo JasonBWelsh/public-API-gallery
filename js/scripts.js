44b8d0aecfe288e1dc12c95cea8a4f763d4d6eae
Jason Welsh <jasonbwelsh@gmail.com>
2017-07-17 17:15:51 -0400

    basic mobile version complete

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const mobileSelect = document.querySelector('.mobile-select');
 let numbers = []; // holds random nums for card indexes 
 const gallerySection = document.querySelector('.gallery-section');
 const galleryList = gallerySection.querySelector('.gallery-list');
+const infoDiv = document.querySelector('.info');
 
 // Generate list of random numbers between 1-100 to use as array indexes
 function getRandomNumbers(x) {
@@ -24,12 +25,16 @@ function buildGallery(cards, numbers) {
 		let imgSrc = cards[curNum].imageUrl;
 		img.setAttribute("src", imgSrc);
 		img.setAttribute("alt", "Randomly generated card");
+		let span = document.createElement("span");
+		span.textContent = cards[curNum].name;
 		li.appendChild(img);
+		li.appendChild(span);
 		galleryList.appendChild(li);
 	}
 }
 
 mobileSelect.addEventListener('click', (e) => {
+	infoDiv.style.display = 'none';
 	// sets .selected class on nav items
 	if (e.target.tagName === 'LI') {
 		let selected = e.target;
@@ -45,6 +50,7 @@ mobileSelect.addEventListener('click', (e) => {
 		const magicRed = "?colors=red";
 		const magicBlack = "?colors=black";
 		const magicWhite = "?colors=white";
+		const magicBlue = "?colors=blue";
 		// check button color and filter server response for matching color set
 		if (color === 'red') {
 			magicAPI += magicRed;
@@ -52,6 +58,8 @@ mobileSelect.addEventListener('click', (e) => {
 			magicAPI += magicBlack;
 		} else if (color === 'white') {
 			magicAPI += magicWhite;
+		} else if (color === 'blue') {
+			magicAPI += magicBlue;
 		}
 		function displayCards(data) {
 			const output = document.querySelector('.output');
@@ -59,7 +67,7 @@ mobileSelect.addEventListener('click', (e) => {
 			const cardList = []; // holds individial cards
 			// loop over random numbers array and use each number for index
 			// number for cardList
-			getRandomNumbers(10); // get specified number of random numbers
+			getRandomNumbers(12); // get specified number of random numbers
 			for (let i = 0; i < numbers.length; i += 1) {
 				let curNum = numbers[i];
 				cardList.push(cards[curNum]);
